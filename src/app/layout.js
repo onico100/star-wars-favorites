bc643a8fb7ba8901599cf93e1dbb6c8f4a2f96b1
onico100 <142163577+onico100@users.noreply.github.com>
2025-01-07 11:06:22 +0200

    more style

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Header from "../components/Header";
 
 export const metadata = {
   title: "Star Wars Favorites",
@@ -9,15 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header
-          style={{
-            padding: "1rem",
-            backgroundColor: "#20232a",
-            color: "white",
-          }}
-        >
-          <h1>Star Wars Movies</h1>
-        </header>
+        <Header />
         {children}
       </body>
     </html>
